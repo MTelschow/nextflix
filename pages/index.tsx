001313f047885bdc570931a1e214ef9ea3d74fa6
Mathis Telschow <mathistelschow@gmail.com>
2023-06-06 16:28:36 +0200

    Remove default index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import styles from '@/styles/Home.module.css';
+
+import Banner from '@/components/banner/banner';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Nextflix</title>
-        <meta name="description" content="Video streaming with next.js" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1>Nextflix</h1>
-      </main>
-    </>
-  )
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Nextflix</title>
+				<meta name='description' content='Video streaming with next.js' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<h1>Nextflix</h1>
+
+      {/* <NavBar /> */}
+      <Banner />
+
+      {/* <Card />  */}
+		</div>
+	);
 }
