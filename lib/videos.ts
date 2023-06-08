b7340a658748d00ef55175e52485d58e348d55a4
Mathis Telschow <mathistelschow@gmail.com>
2023-06-08 15:05:03 +0200

    Add dummy data

--- lib/videos.ts
+++ lib/videos.ts
@@ -1,9 +1,21 @@
+import { items } from '@/data/videos.json';
+
+const data = items.map((item) => {
+    const id = item.id?.videoId || item.id;
+    const snippet = item.snippet;
+    return {
+      title: snippet?.title,
+      imgUrl: `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`,
+      id,
+      description: snippet.description,
+      publishTime: snippet.publishedAt,
+      channelTitle: snippet.channelTitle,
+      statistics: item.statistics ? item.statistics : { viewCount: 0 },
+    };
+  });
+
 const getVideos = (topic: string) => {
-	const video = {
-		id: '12',
-		imgUrl: '/static/clifford.webp',
-	};
-    return new Array(3).fill(video);
+	return data;
 };
 
 const getPopularVideos = () => {
