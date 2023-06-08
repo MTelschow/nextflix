f623027e2b369d4411aa90bfb391c241d08edc05
Mathis Telschow <mathistelschow@gmail.com>
2023-06-08 19:08:01 +0200

    Use youtube api

--- lib/videos.ts
+++ lib/videos.ts
@@ -1,32 +1,103 @@
-import { items } from '@/data/videos.json';
+import videoTestData from '../data/videos.json';
+// import { getMyListVideos, getWatchedVideos } from "./db/hasura";
 
-const data = items.map((item) => {
-    const id = item.id?.videoId || item.id;
-    const snippet = item.snippet;
-    return {
-      title: snippet?.title,
-      imgUrl: `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`,
-      id,
-      description: snippet.description,
-      publishTime: snippet.publishedAt,
-      channelTitle: snippet.channelTitle,
-      statistics: item.statistics ? item.statistics : { viewCount: 0 },
-    };
-  });
+const fetchVideos = async (url: string) => {
+	const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+	const BASE_URL = 'youtube.googleapis.com/youtube/v3';
 
-const getVideos = (topic: string) => {
-	return data;
+	const response = await fetch(
+		`https://${BASE_URL}/${url}&maxResults=25&key=${YOUTUBE_API_KEY}`
+	);
+
+	return await response.json();
 };
 
-const getPopularVideos = () => {
-	return getVideos('poplar');
+export const getCommonVideos = async (url: string) => {
+	try {
+		const isDev = process.env.DEVELOPMENT;
+		const data = isDev ? videoTestData : await fetchVideos(url);
+		if (data?.error) {
+			console.error('Youtube API error', data.error);
+			return [];
+		}
+
+		return data?.items.map(
+			(item: { id: { videoId: any }; snippet: any; statistics: any }) => {
+				const id = item.id?.videoId || item.id;
+				const snippet = item.snippet;
+				return {
+					title: snippet?.title,
+					imgUrl: `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`,
+					id,
+					description: snippet.description,
+					publishTime: snippet.publishedAt,
+					channelTitle: snippet.channelTitle,
+					statistics: item.statistics ? item.statistics : { viewCount: 0 },
+				};
+			}
+		);
+	} catch (error) {
+		console.error('Something went wrong with video library', error);
+		return [];
+	}
 };
 
-const getWatchItAgainVideos = () => {};
+export const getVideos = (searchQuery: string) => {
+	const URL = `search?part=snippet&q=${searchQuery}&type=video`;
+	return getCommonVideos(URL);
+};
 
-interface Video {
-	id: string;
-	imgUrl: string;
-}
+export const getPopularVideos = () => {
+	const URL =
+		'videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US';
 
-export { getVideos, getPopularVideos, getWatchItAgainVideos };
+	//videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc
+	return getCommonVideos(URL);
+};
+
+export const getYoutubeVideoById = (videoId: string) => {
+	const URL = `videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}`;
+
+	return getCommonVideos(URL);
+};
+
+
+
+
+
+
+export const getWatchItAgainVideos = async (userId: any, token: any) => {
+	const videos = await getPopularVideos();
+	return (
+		videos?.map((video: { videoId: any }) => {
+			return {
+				id: video.videoId,
+				imgUrl: `https://i.ytimg.com/vi/${video.videoId}/maxresdefault.jpg`,
+			};
+		}) || []
+	);
+};
+
+// export const getWatchItAgainVideos = async (userId: any, token: any) => {
+//   const videos = await getWatchedVideos(userId, token);
+//   return (
+//     videos?.map((video: { videoId: any; }) => {
+//       return {
+//         id: video.videoId,
+//         imgUrl: `https://i.ytimg.com/vi/${video.videoId}/maxresdefault.jpg`,
+//       };
+//     }) || []
+//   );
+// };
+
+// export const getMyList = async (userId: any, token: any) => {
+//   const videos = await getMyListVideos(userId, token);
+//   return (
+//     videos?.map((video: { videoId: any; }) => {
+//       return {
+//         id: video.videoId,
+//         imgUrl: `https://i.ytimg.com/vi/${video.videoId}/maxresdefault.jpg`,
+//       };
+//     }) || []
+//   );
+// };
