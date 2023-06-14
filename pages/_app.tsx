4c41083f18f11a9c3a4689e4bdc84789c389520a
Mathis Telschow <mathistelschow@gmail.com>
2023-06-14 16:30:22 +0200

    reformat

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,46 +1,42 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { magic } from "../lib/magic-client";
-import Loading from "../components/loading/loading";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { magic } from '../lib/magic-client';
+import Loading from '../components/loading/loading';
 
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
+	const router = useRouter();
+	const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const handleLoggedIn = async () => {
-      if (!magic) return router.push('/login')
-      const isLoggedIn = await magic.user.isLoggedIn();
-      if (isLoggedIn) {
-        // route to /
-        router.push("/");
-      } else {
-        // route to /login
-        router.push("/login");
-      }
-    };
-    handleLoggedIn();
-  }, []);
+	useEffect(() => {
+		const handleLoggedIn = async () => {
+			if (!magic) return router.push('/login');
+			const isLoggedIn = await magic.user.isLoggedIn();
+			if (isLoggedIn) {
+				// route to /
+				router.push('/');
+			} else {
+				// route to /login
+				router.push('/login');
+			}
+		};
+		handleLoggedIn();
+	}, []);
 
-  useEffect(() => {
-    const handleComplete = () => {
-      setIsLoading(false);
-    };
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
+	useEffect(() => {
+		const handleComplete = () => {
+			setIsLoading(false);
+		};
+		router.events.on('routeChangeComplete', handleComplete);
+		router.events.on('routeChangeError', handleComplete);
 
-    return () => {
-      router.events.off("routeChangeComplete", handleComplete);
-      router.events.off("routeChangeError", handleComplete);
-    };
-  }, [router]);
-  return isLoading ? <Loading /> : <Component {...pageProps} />;
-
+		return () => {
+			router.events.off('routeChangeComplete', handleComplete);
+			router.events.off('routeChangeError', handleComplete);
+		};
+	}, [router]);
+	return isLoading ? <Loading /> : <Component {...pageProps} />;
 }
-
-
-
