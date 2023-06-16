e8f6a859ae5bf9d5a051fedddd0d530513d4f343
Mathis Telschow <mathistelschow@gmail.com>
2023-06-16 13:27:47 +0200

    Reformat code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,87 +1,87 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
-import Banner from "../components/banner/banner";
-import NavBar from "../components/nav/navbar";
+import Banner from '../components/banner/banner';
+import NavBar from '../components/nav/navbar';
 
-import SectionCards from "../components/card/section-cards";
+import SectionCards from '../components/card/section-cards';
 
 import {
-  getVideos,
-  getPopularVideos,
-  getWatchItAgainVideos,
-} from "../lib/videos";
-import { redirectUser } from "../utils/redirectUser";
+	getVideos,
+	getPopularVideos,
+	getWatchItAgainVideos,
+} from '../lib/videos';
+import { redirectUser } from '../utils/redirectUser';
 
 export async function getServerSideProps(context: any) {
-  const { userId, token } = await redirectUser(context);
+	const { userId, token } = await redirectUser(context);
 
-  if (!userId) {
-    return {
-      props: {},
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
-  const watchItAgainVideos = await getWatchItAgainVideos(userId, token);
+	if (!userId) {
+		return {
+			props: {},
+			redirect: {
+				destination: '/login',
+				permanent: false,
+			},
+		};
+	}
+	const watchItAgainVideos = await getWatchItAgainVideos(userId, token);
 
-  const disneyVideos = await getVideos("disney trailer");
-  const productivityVideos = await getVideos("Productivity");
+	const disneyVideos = await getVideos('disney trailer');
+	const productivityVideos = await getVideos('Productivity');
 
-  const chessVideos = await getVideos("chess");
+	const chessVideos = await getVideos('chess');
 
-  const popularVideos = await getPopularVideos();
-  return {
-    props: {
-      disneyVideos,
-      chessVideos,
-      productivityVideos,
-      popularVideos,
-      watchItAgainVideos,
-    },
-  };
+	const popularVideos = await getPopularVideos();
+	return {
+		props: {
+			disneyVideos,
+			chessVideos,
+			productivityVideos,
+			popularVideos,
+			watchItAgainVideos,
+		},
+	};
 }
 
 export default function Home({
-  disneyVideos,
-  chessVideos,
-  productivityVideos,
-  popularVideos,
-  watchItAgainVideos,
+	disneyVideos,
+	chessVideos,
+	productivityVideos,
+	popularVideos,
+	watchItAgainVideos,
 }: any) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Netflix</title>
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Netflix</title>
+			</Head>
 
-      <div className={styles.main}>
-        <NavBar />
-        <Banner
-          videoId="4zH5iYM4wJo"
-          title="Clifford the red dog"
-          subTitle="a very cute dog"
-          imgUrl="/static/clifford.webp"
-        />
+			<div className={styles.main}>
+				<NavBar />
+				<Banner
+					videoId='4zH5iYM4wJo'
+					title='Clifford the red dog'
+					subTitle='a very cute dog'
+					imgUrl='/static/clifford.webp'
+				/>
 
-        <div className={styles.sectionWrapper}>
-          <SectionCards title="Disney" videos={disneyVideos} size="large" />
-          <SectionCards
-            title="Watch it again"
-            videos={watchItAgainVideos}
-            size="small"
-          />
-          <SectionCards title="Chess" videos={chessVideos} size="small" />
-          <SectionCards
-            title="Productivity"
-            videos={productivityVideos}
-            size="medium"
-          />
-          <SectionCards title="Popular" videos={popularVideos} size="small" />
-        </div>
-      </div>
-    </div>
-  );
+				<div className={styles.sectionWrapper}>
+					<SectionCards title='Disney' videos={disneyVideos} size='large' />
+					<SectionCards
+						title='Watch it again'
+						videos={watchItAgainVideos}
+						size='small'
+					/>
+					<SectionCards title='Chess' videos={chessVideos} size='small' />
+					<SectionCards
+						title='Productivity'
+						videos={productivityVideos}
+						size='medium'
+					/>
+					<SectionCards title='Popular' videos={popularVideos} size='small' />
+				</div>
+			</div>
+		</div>
+	);
 }
